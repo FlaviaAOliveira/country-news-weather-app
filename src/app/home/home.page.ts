5d0e45e7f7971ef06519f07c4bdab0479762b9cf
Flavia Oliveira <flavia.alves.oliveira@gmail.com>
2024-12-31 15:20:56 -0300

    Setup structure for Home and Settings page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { settingsOutline } from 'ionicons/icons';
+import { RouterLink} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, RouterLink, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  countryName: string = '';
+  countries: any[] = [];
+
+
+  constructor() {
+    addIcons({ settingsOutline });
+  }
+
+  search() {
+
+  }
 }
---
+++ src/app/settings/settings.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonRadio } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.page.html',
+  styleUrls: ['./settings.page.scss'],
+  standalone: true,
+  imports: [IonRadio, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class SettingsPage implements OnInit {
+  selectedUnit: string = 'metric';
+  
+  constructor() { }
+
+  ngOnInit() {
+    const defaultUnit = localStorage.getItem('units');
+    if (defaultUnit) {
+      this.selectedUnit = defaultUnit;
+    } else {
+      this.selectedUnit = 'metric';
+    }
+  
+}
+
+  unitChange() {
+    localStorage.setItem('units', this.selectedUnit);
+  }
+}
